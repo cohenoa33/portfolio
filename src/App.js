82b48d7d2d2cbd8f243addab2bd2de71a9a025d0
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2021-02-18 21:41:48 -0800

    remove padding from navbar on mobile view

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
   componentWillUnmount() {
     window.removeEventListener("scroll", this.handleScroll);
   }
+
   handleScroll() {
     if (
       document.body.scrollTop > 20 ||
