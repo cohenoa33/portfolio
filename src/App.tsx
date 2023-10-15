9d35163783e5ba6de9980a12d6ee90f0f69ab5ad
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2023-10-15 08:05:04 -0700

    upgrade packages

--- src/App.tsx
+++ src/App.tsx
@@ -3,51 +3,49 @@ import "./App.scss";
 import { HashRouter } from "react-router-dom";
 import { Home, Contact, Resume, Social, Projects, Skills } from "./components";
 
-class App extends React.Component {
-  componentDidMount() {
-    window.addEventListener("scroll", this.handleScroll, { passive: true });
-  }
+export default function App() {
+  // componentDidMount() {
+  //   window.addEventListener("scroll", this.handleScroll, { passive: true });
+  // }
 
-  componentWillUnmount() {
-    window.removeEventListener("scroll", this.handleScroll, false);
-  }
+  // componentWillUnmount() {
+  //   window.removeEventListener("scroll", this.handleScroll, false);
+  // }
 
-  handleScroll() {
-    let navbar = document.getElementById("navbar");
-    if (!navbar) return;
-    if (
-      document.body.scrollTop > 20 ||
-      document.documentElement.scrollTop > 20
-    ) {
-      navbar.style.top = "0";
-    } else {
-      navbar.style.top = "-50px";
-    }
-  }
+  // handleScroll() {
+  //   let navbar = document.getElementById("navbar");
+  //   if (!navbar) return;
+  //   if (
+  //     document.body.scrollTop > 20 ||
+  //     document.documentElement.scrollTop > 20
+  //   ) {
+  //     navbar.style.top = "0";
+  //   } else {
+  //     navbar.style.top = "-50px";
+  //   }
+  // }
 
-  render() {
-    return (
-      <HashRouter>
-        <div>
-          <div id="navbar">
-            <a href="#about">About</a>
-            <a href="#skills">Skills</a>
-            <a href="#projects">Projects</a>
-            <a href="#resume">Resume</a>
-            <a href="#contact">Contact</a>
-          </div>
-          <div className="App">
-            <Social />
-            <Home />
-            <Skills />
-            <Projects />
-            <Resume />
-            <Contact section="contact" />
-          </div>
+  // render() {
+  return (
+    <HashRouter>
+      <div>
+        <div id="navbar">
+          <a href="#about">About</a>
+          <a href="#skills">Skills</a>
+          <a href="#projects">Projects</a>
+          <a href="#resume">Resume</a>
+          <a href="#contact">Contact</a>
         </div>
-      </HashRouter>
-    );
-  }
+        <div className="App">
+          <Social />
+          <Home />
+          <Skills />
+          <Projects />
+          <Resume />
+          <Contact section="contact" />
+        </div>
+      </div>
+    </HashRouter>
+  );
+  // }
 }
-
-export default App;
