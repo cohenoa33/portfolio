feafee83b91562d0e2f38eef53cfb122d34169c0
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2024-02-13 17:23:52 -0800

    remove sass, upgrade all packegs

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.scss";
+import "./index.css";
 import App from "./App";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+// Clear the existing HTML content
+document.body.innerHTML = '<div id="app"></div>';
+
+// Render your React component instead
+const root = createRoot(document.getElementById("app") as HTMLElement);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
